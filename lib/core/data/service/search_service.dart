a1dbe924bd2835d41648bc77bb7533e1206c37ab
Nilton Carlos <niltonfirminofs@gmail.com>
2022-03-16 19:27:58 -0300

    feat_search_module

--- lib/core/data/service/search_service.dart
+++ lib/core/data/service/search_service.dart
@@ -1,21 +1,20 @@
-import 'dart:convert';
 import 'dart:core';
-import 'package:is_it_safe_app/app/modules/main_module/modules/search_module/mock/search_results_mockup.dart';
 import 'package:is_it_safe_app/core/data/service/config/api_service.dart';
 import 'package:is_it_safe_app/core/model/Location.dart';
 
 class SearchService {
   final APIService _service = APIService();
-  //TODO Fazer conexão com a API
 
   Future getLocation() async {
-    //TODO Trocar o mock 
-    final location = jsonDecode(searchResultsMockup);
-    final listLocation = location as List;
+    final _response = await _service.doRequest(
+      RequestConfig(
+        'location/find-all',
+        HttpMethod.get,
+      ),
+    );
 
-    final locations =
-        listLocation.map((location) => Location.fromJson(location)).toList();
-
-    return locations;
+    List<Location> _locations =
+        (_response as List).map((e) => Location.fromJson(e)).toList();
+    return _locations;
   }
 }
