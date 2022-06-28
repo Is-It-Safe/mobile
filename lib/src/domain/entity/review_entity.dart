0a3ce50fed50ea72303b28ed87430bec8eead7a1
estermabel <estermabel2@gmail.com>
2022-06-28 19:30:59 -0300

    Atualizando ResponseGetUser e as entidades de Review e User

--- lib/src/domain/entity/review_entity.dart
+++ lib/src/domain/entity/review_entity.dart
@@ -1,5 +1,6 @@
 import 'package:is_it_safe_app/src/core/constants/double_constants.dart';
 import 'package:is_it_safe_app/src/core/constants/string_constants.dart';
+import 'package:is_it_safe_app/src/domain/entity/location_entity.dart';
 import 'package:is_it_safe_app/src/service/api/modules/profile/response/response_get_user.dart';
 
 class ReviewEntity {
@@ -7,12 +8,14 @@ class ReviewEntity {
   String? review;
   String? impressionStatus;
   double? myGrade;
+  LocationEntity? location;
 
   ReviewEntity({
     required this.id,
     this.review,
     this.impressionStatus,
     this.myGrade,
+    this.location,
   });
 
   factory ReviewEntity.toEntity(ResponseGetUserReview review) {
@@ -21,6 +24,11 @@ class ReviewEntity {
       review: review.review ?? StringConstants.empty,
       impressionStatus: review.impressionStatus ?? StringConstants.empty,
       myGrade: review.myGrade ?? DoubleConstants.empty,
+      location: LocationEntity(
+        id: review.locationId!,
+        name: review.locationName ?? StringConstants.empty,
+        address: review.locationAddress ?? StringConstants.empty,
+      ),
     );
   }
 }
--- lib/src/service/api/modules/profile/response/response_get_user.dart
+++ lib/src/service/api/modules/profile/response/response_get_user.dart
@@ -1,19 +1,23 @@
 class ResponseGetUser {
   int? id;
   String? profilePhoto;
+  String? name;
   String? nickname;
   String? pronoun;
   String? gender;
   String? orientation;
+  String? birthDate;
   List<ResponseGetUserReview>? reviews;
 
   ResponseGetUser({
     this.id,
     this.profilePhoto,
+    this.name,
     this.nickname,
     this.pronoun,
     this.gender,
     this.orientation,
+    this.birthDate,
     this.reviews,
   });
 
@@ -25,10 +29,12 @@ class ResponseGetUser {
     return ResponseGetUser(
       id: json['id'],
       profilePhoto: json['profilePhoto'],
+      name: json['fullName'],
       nickname: json['nickname'],
       pronoun: json['pronoun'],
       gender: json['gender'],
       orientation: json['orientation'],
+      birthDate: json['birthDate'],
       reviews: reviews,
     );
   }
@@ -40,19 +46,29 @@ class ResponseGetUserReview {
   String? impressionStatus;
   double? myGrade;
 
+  int? locationId;
+  String? locationName;
+  String? locationAddress;
+
   ResponseGetUserReview({
     this.id,
     this.review,
     this.impressionStatus,
     this.myGrade,
+    this.locationId,
+    this.locationName,
+    this.locationAddress,
   });
 
   factory ResponseGetUserReview.fromJson(dynamic json) {
     return ResponseGetUserReview(
-      id: json['id'],
+      id: json['review_id'],
       review: json['review'],
       impressionStatus: json['impressionStatus'],
       myGrade: json['myGrade'],
+      locationId: json['locationId'],
+      locationName: json['locationName'],
+      locationAddress: json['locationAddress'],
     );
   }
 }
