5c5d65fc12b93b69a4ff5ee37fdc53b8151f9177
Nilton Carlos <niltonfirminofs@gmail.com>
2022-07-18 19:44:45 -0300

    feat: added factory in response_get_location_by_id

--- lib/src/service/api/modules/location/response/response_get_location_by_id.dart
+++ lib/src/service/api/modules/location/response/response_get_location_by_id.dart
@@ -1,15 +1,15 @@
 import 'response_review.dart';
 
-class LocationByID {
+class ResponseGetLocationsById {
   int? id;
   String? name;
   String? endereco;
   String? type;
   String? imgUrl;
   String? averageGrade;
-  List<Reviews>? reviews;
+  List<ResponseReview>? reviews;
 
-  LocationByID(
+  ResponseGetLocationsById(
       {this.id,
       this.name,
       this.endereco,
@@ -18,33 +18,18 @@ class LocationByID {
       this.averageGrade,
       this.reviews});
 
-  LocationByID.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    endereco = json['endereco'];
-    type = json['type'];
-    imgUrl = json['imgUrl'];
-    averageGrade = json['averageGrade'];
-    if (json['reviews'] != null) {
-      reviews = <Reviews>[];
-      json['reviews'].forEach((v) {
-        reviews!.add(new Reviews.fromJson(v));
-      });
-    }
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['endereco'] = this.endereco;
-    data['type'] = this.type;
-    data['imgUrl'] = this.imgUrl;
-    data['averageGrade'] = this.averageGrade;
-    if (this.reviews != null) {
-      data['reviews'] = this.reviews!.map((v) => v.toJson()).toList();
-    }
-    return data;
+  factory ResponseGetLocationsById.fromJson(Map<String, dynamic> json) {
+    return ResponseGetLocationsById(
+      id: json['id'],
+      name: json['name'],
+      endereco: json['endereco'],
+      type: json['type'],
+      imgUrl: json['imgUrl'],
+      averageGrade: json['averageGrade'],
+      reviews: json['reviews'] != null
+          ? (json['reviews'] as List).map((i) => ResponseReview.fromJson(i)).toList()
+          : null,
+    );
   }
 }
 
