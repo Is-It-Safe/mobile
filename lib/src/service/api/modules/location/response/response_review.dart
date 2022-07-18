63c704679f78f9e21b1153fcd12a6713890c2aab
Nilton Carlos <niltonfirminofs@gmail.com>
2022-07-18 19:45:21 -0300

    feat: added factory in response_review

--- lib/src/service/api/modules/location/response/response_review.dart
+++ lib/src/service/api/modules/location/response/response_review.dart
@@ -1,27 +1,25 @@
-class Reviews {
+class ResponseReview {
   int? id;
   String? author;
   String? review;
   String? createdAt;
   double? myGrade;
 
-  Reviews({this.id, this.author, this.review, this.createdAt, this.myGrade});
+  ResponseReview({
+    this.id,
+    this.author,
+    this.review,
+    this.createdAt,
+    this.myGrade,
+  });
 
-  Reviews.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    author = json['author'];
-    review = json['review'];
-    createdAt = json['createdAt'];
-    myGrade = json['myGrade'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['author'] = this.author;
-    data['review'] = this.review;
-    data['createdAt'] = this.createdAt;
-    data['myGrade'] = this.myGrade;
-    return data;
+  factory ResponseReview.fromJson(Map<String, dynamic> json) {
+    return ResponseReview(
+      id: json['id'],
+      author: json['author'],
+      review: json['review'],
+      createdAt: json['createdAt'],
+      myGrade: json['myGrade'],
+    );
   }
 }
