50618b157270374969949fcf78d7699b0395eb19
Nilton Carlos <niltonfirminofs@gmail.com>
2022-08-09 07:25:46 -0300

    feat: refactor review_module

--- lib/src/app/modules/location/review/review_module.dart
+++ lib/src/app/modules/location/review/review_module.dart
@@ -1,22 +1,23 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:is_it_safe_app/src/service/api/modules/location/location_service.dart';
 
 import 'presenter/bloc/review_bloc.dart';
 import 'presenter/pages/review_page.dart';
 
-
 class ReviewModule extends Module {
-
   @override
   final List<Bind> binds = [
     Bind.lazySingleton((i) => ReviewBloc()),
-    Bind.lazySingleton((i) => ReviewService()),
+    Bind.lazySingleton((i) => LocationService()),
   ];
 
   @override
   final List<ModularRoute> routes = [
-    ChildRoute(Modular.initialRoute,
-        child: (context, args) =>  ReviewWidget(args: args.data, )),
+    ChildRoute(
+      Modular.initialRoute,
+      child: (context, args) => ReviewWidget(
+        args: args.data,
+      ),
+    ),
   ];
-
-  
 }
