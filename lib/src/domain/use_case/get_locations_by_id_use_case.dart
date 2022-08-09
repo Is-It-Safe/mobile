2f25c9cad3a252d54c6d7727a25ee8bf20ab7126
Nilton Carlos <niltonfirminofs@gmail.com>
2022-08-09 07:24:49 -0300

    feat: create get_locations_by_id_use_case

---
+++ lib/src/domain/use_case/get_locations_by_id_use_case.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:is_it_safe_app/src/core/interfaces/safe_use_case.dart';
+import 'package:is_it_safe_app/src/domain/entity/location_entity.dart';
+import 'package:is_it_safe_app/src/service/api/modules/location/location_service.dart';
+import 'package:is_it_safe_app/src/service/api/modules/location/location_service_interface.dart';
+
+class GetLocationsByIdUseCase extends SafeUseCase {
+  late final ILocationService _service;
+
+  GetLocationsByIdUseCase() {
+    _service = Modular.get<LocationService>();
+  }
+
+  Future<List<LocationEntity>> call(int id) async {
+    final _response = await _service.getLocationById( locationId: id);
+
+    return _response?.map((e) => LocationEntity.toEntity(e)).toList() ?? [];
+  }
+}
