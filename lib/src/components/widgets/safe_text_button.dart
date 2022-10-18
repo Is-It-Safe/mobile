92e396639849f64617c2a1df455d7089ad160efc
Flávio Melo <hastnumber1@gmail.com>
2022-10-17 22:00:51 -0300

    Update safe_text_button.dart
    feitas as alterações solicitadas: colocado um onTap, sequência preText Text PosText, mudança no style.

--- lib/src/components/widgets/safe_text_button.dart
+++ lib/src/components/widgets/safe_text_button.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:is_it_safe_app/src/components/style/colors/safe_colors.dart';
 import 'package:is_it_safe_app/src/components/style/text/text_styles.dart';
 import 'package:is_it_safe_app/src/core/constants/string_constants.dart';
 
 class SafeTextButton extends StatelessWidget {
   final String text;
-  void Function()? onTapSign;
-  void Function()? onTapPass;
+  void Function()? onTap;
   final String? preText;
   final String? posText;
   final Color? textColor;
@@ -15,8 +13,7 @@ class SafeTextButton extends StatelessWidget {
   SafeTextButton(
       {Key? key,
       required this.text,
-      required this.onTapSign,
-      required this.onTapPass,
+      this.onTap,
       this.preText,
       this.posText,
       this.textColor,
@@ -26,48 +23,49 @@ class SafeTextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: [
         _firstText(context),
         _secondText(context),
+        _thirdText(context),
       ],
     );
   }
 
   Widget _firstText(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          preText ?? StringConstants.empty,
-          style: TextStyles.bodyText1(
-            color: allTextColor,
-          ),
-        ),
-        GestureDetector(
-          onTap: onTapSign,
-          child: Text(
-            posText ?? StringConstants.empty,
-            style: TextStyles.bodyText1(
-              color: textColor,
-              textDecoration: TextDecoration.underline,
-            ),
-          ),
-        ),
-      ],
+    return Text(
+      preText ?? StringConstants.empty,
+      //TODO: corrigir o style
+      style:
+          textStyle != null ? null : TextStyles.bodyText1(color: allTextColor),
+      //style: TextStyles.bodyText1(color: allTextColor),
     );
   }
 
   Widget _secondText(BuildContext context) {
     return GestureDetector(
-      onTap: onTapPass,
+      onTap: onTap,
       child: Text(
         text,
-        style: TextStyles.bodyText1(
-          color: allTextColor,
-          textDecoration: TextDecoration.underline,
-        ),
+        //TODO: corrigir o style
+        style: textStyle != null
+            ? null
+            : TextStyles.bodyText1(
+                color: textColor, textDecoration: TextDecoration.underline),
+        //style: TextStyles.bodyText1(color: textColor, textDecoration: TextDecoration.underline),
       ),
+    );
+  }
+
+  Widget _thirdText(BuildContext context) {
+    return Text(
+      posText ?? StringConstants.empty,
+      //TODO: corrigir o style
+      style: textStyle != null
+          ? null
+          : TextStyles.bodyText1(
+              color: allTextColor, textDecoration: TextDecoration.underline),
+      //style: TextStyles.bodyText1(color: allTextColor),
     );
   }
 }
