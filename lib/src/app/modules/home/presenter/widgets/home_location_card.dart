c34653dda401dbcebffeab9f5fe227e4c048b7eb
Felipe Ribeiro <feliper.dev@gmail.com>
2023-01-11 17:49:50 -0300

    fix: Ajustes no HomeLocationCard

--- lib/src/app/modules/home/presenter/widgets/home_location_card.dart
+++ lib/src/app/modules/home/presenter/widgets/home_location_card.dart
@@ -20,28 +20,47 @@ class HomeLocationCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        width: MediaQuery.of(context).size.width,
+        width: size.width,
         decoration: BoxDecoration(
           color: SafeColors.generalColors.primary,
           borderRadius: BorderRadius.circular(8.0),
         ),
-        child: Column(
-          children: [
-            _mountLocationPicture(context),
-            SizedBox(
-              height: location.imagePath?.isNotEmpty == true ? 0.0 : 16.0,
-            ),
-            _mountBody(context),
-          ],
-        ),
+        child: LayoutBuilder(builder: (context, constraints) {
+          return Stack(
+            children: [
+              Column(
+                children: [
+                  _LocationPicture(location: location),
+                  SizedBox(
+                    height: location.imagePath?.isNotEmpty == true ? 0.0 : 16.0,
+                  ),
+                  _LocationBody(location: location, size: size),
+                ],
+              ),
+              Positioned(
+                left: constraints.maxWidth * .058,
+                bottom: size.height * .073,
+                child: _LocationGrade(location: location),
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
+}
 
-  Widget _mountLocationPicture(BuildContext context) {
+class _LocationPicture extends StatelessWidget {
+  final LocationEntity location;
+  const _LocationPicture({Key? key, required this.location}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     if (location.imagePath?.isNotEmpty == true) {
       return Container(
         margin: const EdgeInsets.all(16.0),
@@ -65,14 +84,21 @@ class HomeLocationCard extends StatelessWidget {
       return const SizedBox.shrink();
     }
   }
+}
 
-  Widget _mountBody(BuildContext context) {
+class _LocationBody extends StatelessWidget {
+  final LocationEntity location;
+  final Size size;
+  const _LocationBody({Key? key, required this.location, required this.size})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        _mountGrade(),
-        const SizedBox(width: 16),
+        const Spacer(),
         Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.start,
@@ -86,23 +112,7 @@ class HomeLocationCard extends StatelessWidget {
             const SizedBox(height: 8),
           ],
         ),
-      ],
-    );
-  }
-
-  Widget _mountGrade() {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        SvgPicture.asset(
-          AssetConstants.icons.star,
-          height: 35,
-          width: 35,
-        ),
-        Text(
-          location.averageGrade.toString(),
-          style: TextStyles.headline3(),
-        ),
+        SizedBox(width: size.width * .08),
       ],
     );
   }
@@ -143,6 +153,29 @@ class HomeLocationCard extends StatelessWidget {
               StringConstants.space +
               S.current.textReviews,
           style: TextStyles.helper(color: SafeColors.textColors.dark),
+        ),
+      ],
+    );
+  }
+}
+
+class _LocationGrade extends StatelessWidget {
+  final LocationEntity location;
+  const _LocationGrade({Key? key, required this.location}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SvgPicture.asset(
+          AssetConstants.icons.star,
+          height: 35,
+          width: 35,
+        ),
+        Text(
+          location.averageGrade.toString(),
+          style: TextStyles.headline3(),
         ),
       ],
     );
