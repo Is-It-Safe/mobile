e521ccc3c19dbe67b20f5d7a32b95d7d55504655
estermabel <estermabel2@gmail.com>
2023-05-09 00:03:37 -0300

    Fix: Get location from user

--- lib/src/app/modules/home/presenter/VOs/home_drawer_vo.dart
+++ lib/src/app/modules/home/presenter/VOs/home_drawer_vo.dart
@@ -1,3 +1,5 @@
+import 'package:is_it_safe_app/src/core/constants/string_constants.dart';
+
 class HomeDrawerVO {
   String userName;
   String userImage;
@@ -6,4 +8,19 @@ class HomeDrawerVO {
     required this.userName,
     required this.userImage,
   });
+
+  HomeDrawerVO copyWith({
+    String? userName,
+    String? userImage,
+  }) {
+    return HomeDrawerVO(
+      userName: userName ?? this.userName,
+      userImage: userImage ?? this.userImage,
+    );
+  }
+
+  static HomeDrawerVO get empty => HomeDrawerVO(
+        userName: StringConstants.empty,
+        userImage: StringConstants.empty,
+      );
 }
