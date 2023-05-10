015413cf8d6f5218ce54fdc6937c74a8b0f03118
estermabel <estermabel2@gmail.com>
2023-05-10 12:03:14 -0300

    Refactor: Removendo alguns problems

--- lib/src/components/widgets/safe_text_form_field.dart
+++ lib/src/components/widgets/safe_text_form_field.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:is_it_safe_app/generated/l10n.dart';
