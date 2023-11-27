5705a2d9ee978b47a8e102891d9ace1c4330a58b
dev <adriana.bispo283@gmail.com>
2023-11-26 21:22:39 -0300

    atualizado o texto de textSuccessSaveLocation

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -1163,7 +1163,7 @@ class S {
   /// `Local enviado para revisão com sucesso!`
   String get textSuccessSaveLocation {
     return Intl.message(
-      'Local enviado para revisão com sucesso!',
+      'Estamos processando os detalhes do estabelecimento para tornar sua experiência ainda mais incrível. Em breve, ele estará visível para todos!',
       name: 'textSuccessSaveLocation',
       desc: '',
       args: [],
