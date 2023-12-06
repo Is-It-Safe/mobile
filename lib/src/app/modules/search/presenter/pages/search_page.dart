c155af7cfd5c6ee9dac69ac6577862654a3c8c81
dev <adriana.bispo283@gmail.com>
2023-12-05 22:26:04 -0300

    limpar campo de pesquisa

--- lib/src/app/modules/search/presenter/pages/search_page.dart
+++ lib/src/app/modules/search/presenter/pages/search_page.dart
@@ -63,9 +63,11 @@ class _SearchPageState extends SafeState<SearchPage, SearchBloc> {
                       children: [
                         SafeEmptyCard.search(),
                         SafeTextButton(
-                          text: S.current.textAddLocation,
-                          onTap: bloc.navigateToAddLocationPage,
-                        ),
+                            text: S.current.textAddLocation,
+                            onTap: () {
+                              bloc.navigateToAddLocationPage();
+                              bloc.searchTextController.clear();
+                            }),
                       ],
                     );
                   }
