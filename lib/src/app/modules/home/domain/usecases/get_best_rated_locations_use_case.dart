a285568cda9eeb6b89c6c8535232082e3f0ba262
estermabel <estermabel2@gmail.com>
2024-01-07 16:47:36 -0300

    Fix: Quando a cidade não fornecer resultados, trás os resultados gerais

--- lib/src/app/modules/home/domain/usecases/get_best_rated_locations_use_case.dart
+++ lib/src/app/modules/home/domain/usecases/get_best_rated_locations_use_case.dart
@@ -1,3 +1,4 @@
+import 'package:is_it_safe_app/src/app/modules/home/domain/models/response/response_get_best_rated_places.dart';
 import 'package:is_it_safe_app/src/app/modules/home/services/home_services_interface.dart';
 import 'package:is_it_safe_app/src/core/interfaces/safe_use_case.dart';
 import 'package:is_it_safe_app/src/app/modules/location/domain/entities/location_entity.dart';
@@ -8,7 +9,12 @@ class GetBestRatedLocationsUseCase extends SafeUseCase {
   GetBestRatedLocationsUseCase(this.service);
 
   Future<List<LocationEntity>> call({String? place}) async {
-    final response = await service.getBestRatedPlaces(place);
+    List<ResponseGetRatedPlaces> response =
+        await service.getBestRatedPlaces(place);
+
+    if (response.isEmpty) {
+      response = await service.getBestRatedPlaces(null);
+    }
     return response
         .map((e) => LocationEntity.fromGetBestRatedPlaces(e))
         .toList();
