bb6e4498ad8c15cc6c414bed249cf0ea0c986b44
Tharsis Lamin <tharsisboamorte@hotmail.com>
2024-03-18 18:52:11 -0300

    Refactor[441] Ajuste para tratamento de error no splash

--- lib/src/app/modules/configuration/account/domain/usecases/get_user_login_use_case.dart
+++ lib/src/app/modules/configuration/account/domain/usecases/get_user_login_use_case.dart
@@ -1,19 +1,21 @@
+import 'package:is_it_safe_app/src/app/modules/auth/error/safe_auth_error.dart';
 import 'package:is_it_safe_app/src/core/interfaces/safe_use_case.dart';
 import 'package:is_it_safe_app/src/core/util/safe_log_util.dart';
 import 'package:is_it_safe_app/src/service/shared_preferences/shared_preferences_service_interface.dart';
+import 'package:result_dart/result_dart.dart';
 
 class GetUserLoginUseCase implements SafeUseCase {
   final ISharedPreferencesService sharedPreferences;
 
   GetUserLoginUseCase(this.sharedPreferences);
 
-  Future<bool> call() async {
+  Future<Result<bool, SafeAuthError>> call() async {
     try {
       final response = await sharedPreferences.readLogin();
-      return response;
-    } catch (e) {
+      return Success(response);
+    } on SafeAuthError catch (e) {
       SafeLogUtil.instance.logError(e);
+      return Failure(e);
     }
-    return false;
   }
 }
